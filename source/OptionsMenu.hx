9a664541c20cb599d58089beed49743c1e02df21
kayisfunne <78283751+kayisfunne@users.noreply.github.com>
2021-06-20 17:57:53 -0700

    Create OptionsMenu.hx
    Le troll

---
+++ source/OptionsMenu.hx
@@ -0,0 +1,212 @@
+package;
+
+import Controls.KeyboardScheme;
+import Controls.Control;
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.addons.display.FlxGridOverlay;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.input.keyboard.FlxKey;
+import flixel.math.FlxMath;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import lime.utils.Assets;
+
+class OptionsMenu extends MusicBeatState
+{
+	var selector:FlxText;
+	var curSelected:Int = 0;
+	var info:FlxText;
+
+	var controlsStrings:Array<String> = [];
+	var inputStr:Array<String> = ["W A S D", "D F J K"];
+
+	private var grpControls:FlxTypedGroup<Alphabet>;
+	var versionShit:FlxText;
+	override function create()
+	{
+		/*
+		FlxG.save.data.dfjk = 0;
+		FlxG.save.data.offset = 0;
+		FlxG.save.data.volume = 0.5;
+		FlxG.save.flush();
+		*/
+
+		var menuBG:FlxSprite = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
+		controlsStrings = CoolUtil.coolStringFile(inputStr[FlxG.save.data.dfjk] + "\n" + (FlxG.save.data.newInput ? "UnPog input easy" : "Pog input normal") + "\n" + (FlxG.save.data.downscroll ? 'Downscroll' : 'Upscroll') + "\nAccuracy " + (!FlxG.save.data.accuracyDisplay ? "off" : "on") + "\n" + (FlxG.save.data.mash_punish ? 'Anti-Mash on' : 'Anti-Mash off'));
+		
+		trace(controlsStrings);
+
+		menuBG.color = 0xFFea71fd;
+		menuBG.setGraphicSize(Std.int(menuBG.width * 1.1));
+		menuBG.updateHitbox();
+		menuBG.screenCenter();
+		menuBG.antialiasing = true;
+
+		add(menuBG);
+
+		grpControls = new FlxTypedGroup<Alphabet>();
+		add(grpControls);
+
+		for (i in 0...controlsStrings.length)
+		{
+				var controlLabel:Alphabet = new Alphabet(0, (70 * i) + 30, controlsStrings[i], true, false);
+				controlLabel.isMenuItem = true;
+				controlLabel.targetY = i;
+				grpControls.add(controlLabel);
+			// DONT PUT X IN THE FIRST PARAMETER OF new ALPHABET() !!
+		}
+
+
+		versionShit = new FlxText(5, FlxG.height - 18, 0, "Offset (Left, Right): " + FlxG.save.data.offset, 12);
+		versionShit.scrollFactor.set();
+		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		add(versionShit);
+
+		super.create();
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+
+			if (controls.BACK)
+				FlxG.switchState(new MainMenuState());
+			if (controls.UP_P)
+				changeSelection(-1);
+			if (controls.DOWN_P)
+				changeSelection(1);
+			
+			if (controls.RIGHT_P)
+			{
+				FlxG.save.data.offset++;
+				versionShit.text = "Offset (Left, Right): " + FlxG.save.data.offset;
+			}
+
+			if (controls.LEFT_P)
+			{
+				FlxG.save.data.offset--;
+				versionShit.text = "Offset (Left, Right): " + FlxG.save.data.offset;
+			}
+
+			if (controls.RIGHT_R || controls.LEFT_R)
+			{
+				FlxG.save.flush();
+			}
+	
+			remove(info);
+			switch (curSelected)
+			{
+				case 1:
+					var infoTxt:String = "No miss-press punishment";
+
+					if (!FlxG.save.data.newInput)
+					{
+						infoTxt = "Lmao)";
+					}
+
+					info = new FlxText(108, FlxG.height - 290, 0, infoTxt, 20);
+					info.scrollFactor.set();
+					info.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+					add(info);
+				case 4:
+					var infoTxt:String = "Only acts on unpog Input";
+
+					info = new FlxText(108, FlxG.height - 290, 0, infoTxt, 20);
+					info.scrollFactor.set();
+					info.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+					add(info);
+			}
+			if (controls.ACCEPT)
+			{
+				grpControls.remove(grpControls.members[curSelected]);
+				switch(curSelected)
+				{
+					case 0:
+						FlxG.save.data.dfjk ++;
+						if (FlxG.save.data.dfjk > 2)
+						{
+							FlxG.save.data.dfjk = 0;
+						}
+						
+						if (FlxG.save.data.dfjk == 1)
+							controls.setKeyboardScheme(KeyboardScheme.Solo, true);
+						else if (FlxG.save.data.dfjk == 2)
+							controls.setKeyboardScheme(KeyboardScheme.Woops, true);
+						else
+							controls.setKeyboardScheme(KeyboardScheme.Duo(true), true);
+
+						var inTxt:String = inputStr[FlxG.save.data.dfjk];
+
+						var ctrl:Alphabet = new Alphabet(0, (70 * curSelected) + 30, inTxt, true, false);
+						ctrl.isMenuItem = true;
+						ctrl.targetY = curSelected;
+						grpControls.add(ctrl);
+						
+					case 1:
+						FlxG.save.data.newInput = !FlxG.save.data.newInput;
+						var ctrl:Alphabet = new Alphabet(0, (70 * curSelected) + 30, (FlxG.save.data.newInput ? "UnPog input easy" : "Pog input normal"), true, false);
+						ctrl.isMenuItem = true;
+						ctrl.targetY = curSelected - 1;
+						grpControls.add(ctrl);
+					case 2:
+						FlxG.save.data.downscroll = !FlxG.save.data.downscroll;
+						var ctrl:Alphabet = new Alphabet(0, (70 * curSelected) + 30, (FlxG.save.data.downscroll ? 'Downscroll' : 'Upscroll'), true, false);
+						ctrl.isMenuItem = true;
+						ctrl.targetY = curSelected - 2;
+						grpControls.add(ctrl);
+					case 3:
+						FlxG.save.data.accuracyDisplay = !FlxG.save.data.accuracyDisplay;
+						var ctrl:Alphabet = new Alphabet(0, (70 * curSelected) + 30, "Accuracy " + (!FlxG.save.data.accuracyDisplay ? "off" : "on"), true, false);
+						ctrl.isMenuItem = true;
+						ctrl.targetY = curSelected - 3;
+						grpControls.add(ctrl);
+					case 4:
+						FlxG.save.data.mash_punish = !FlxG.save.data.mash_punish;
+						var ctrl:Alphabet = new Alphabet(0, (70 * curSelected) + 30, (FlxG.save.data.mash_punish ? "Anti-Mash on" : "Anti-Mash off"), true, false);
+						ctrl.isMenuItem = true;
+						ctrl.targetY = curSelected - 4;
+						grpControls.add(ctrl);
+				}
+				FlxG.save.flush();
+			}
+	}
+
+	var isSettingControl:Bool = false;
+
+	function changeSelection(change:Int = 0)
+	{
+		#if !switch
+		// NGio.logEvent('Fresh');
+		#end
+		
+		FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
+
+		curSelected += change;
+
+		if (curSelected < 0)
+			curSelected = grpControls.length - 1;
+		if (curSelected >= grpControls.length)
+			curSelected = 0;
+
+		// selector.y = (70 * curSelected) + 30;
+
+		var bullShit:Int = 0;
+
+		for (item in grpControls.members)
+		{
+			item.targetY = bullShit - curSelected;
+			bullShit++;
+
+			item.alpha = 0.6;
+			// item.setGraphicSize(Std.int(item.width * 0.8));
+
+			if (item.targetY == 0)
+			{
+				item.alpha = 1;
+				// item.setGraphicSize(Std.int(item.width));
+			}
+		}
+	}
+}
