5828171b141cf0c70378c61cd2c5f94ab4a3a787
Gede Hari <harikresnagede@gmail.com>
2021-06-29 15:57:08 +0800

    Improved Conductor class, now features interpolated time for smooth note rendering

--- source/Conductor.hx
+++ source/Conductor.hx
@@ -2,17 +2,18 @@ package;
 
 import flixel.FlxBasic;
 import flixel.FlxG;
-import flixel.system.FlxSound;
+import flixel.util.FlxTimer;
 
 class Conductor extends FlxBasic
 {
-	// Song information
+	// Music information
 	public static var bpm:Int = 100;
 	public static var bars:Int = 4;
 	public static var ticksPerBeat:Int = 4;
 
 	// Tracking variables
 	public static var time:Float = 0;
+	public static var interpTime:Float = 0;
 	static var prevTime:Float = 0;
 	public static var beat:Int = 0;
 	static var prevBeat:Int = -1;
@@ -22,7 +23,11 @@ class Conductor extends FlxBasic
 	public static var globalOffset:Float = 0;
 	public static var songOffset:Float = 0;
 
+	// Callback to call when a beat changes
 	public var onBeat:() -> Void;
+
+	// Timer to update interpTime
+	var interpTimeUpdateTimer:FlxTimer;
 
 	public function new()
 	{
@@ -35,7 +40,39 @@ class Conductor extends FlxBasic
 
 		// Update time
 		if (FlxG.sound.music != null && FlxG.sound.music.playing == true)
+		{
 			time = FlxG.sound.music.time / 1000;
+
+			// Interpolated time used for smooth visuals (like note rendering)
+			interpTime += elapsed;
+			if (interpTimeUpdateTimer == null)
+			{
+				interpTime = time;
+				// Update interpTime to match actual time every 500 ms
+				interpTimeUpdateTimer = new FlxTimer().start(0.5, function(timer:FlxTimer)
+				{
+					interpTime = time;
+					timer.reset(0.5);
+				});
+
+				// Also update interpTime when the frame delta is too high (below 10 FPS-ish)
+				if (elapsed >= 0.1)
+				{
+					interpTime = time;
+				}
+			}
+		}
+		else
+		{
+			if (interpTimeUpdateTimer != null)
+			{
+				interpTimeUpdateTimer.cancel();
+				interpTimeUpdateTimer = null;
+			}
+
+			// Always match with actual time when music stopped
+			interpTime = time;
+		}
 
 		// Update beat
 		var beatF:Float = time * bpm / 60.0;
